e61f330f0103eabeaf51a2f5198a730ec7a15a23
Your Name <you@example.com>
2023-07-08 07:51:47 -0700

    updated

--- 0x09-static_libraries/main.c
+++ 0x09-static_libraries/main.c
@@ -1,8 +1,16 @@
 #include "main.h"
 
+/*
+ * main - Entry point of the program
+ *
+ * This function prints a quote by Kevin Mitnick.
+ *
+ * Return: Always 0 (indicating successful execution)
+ */
+
 int main(void)
 {
-	_puts("\"At the end of the day,
-		       	my goal was to be the best hacker\"\n\t- Kevin Mitnick");
+	_puts("\"At the end of the day, my goal was to be the best hacker\"\n"
+			"\t- Kevin Mitnick");
 	return (0);
 }
